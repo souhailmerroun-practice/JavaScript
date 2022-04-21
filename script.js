be7dfa0a8001640fb13da116812e3b2a55895c6a
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-04-21 20:12:36 +0200

    Testing display in the title head section

--- script.js
+++ script.js
@@ -17,12 +17,13 @@ var x = setInterval(function () {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+    document.getElementById("paragraphTag").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    document.getElementById("headTitleTag").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
     // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("countdown").innerHTML = "EXPIRED";
+        document.getElementById("paragraphTag").innerHTML = "EXPIRED";
+        document.getElementById("headTitleTag").innerHTML = "EXPIRED";
     }
 }, 1000);
